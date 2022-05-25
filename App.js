9801333e7f04fa2c187302f19822788128610910
Nazim-hasan <nazimhasan124073@gmail.com>
2022-05-26 01:06:04 +0600

    Fonts Added

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Colors } from "./src/Theme/Colors";
 import { Spacing } from "./src/Theme/Spacing";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Antonio-Medium": require("./assets/Fonts/Antonio-Medium.ttf"),
+    "Spartan-Bold": require("./assets/Fonts/Spartan-Bold.ttf"),
+    "Spartan-Regular": require("./assets/Fonts/Spartan-Regular.ttf"),
+  });
+  if (!loaded) {
+    <Text>Font is Loading...</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "Spartan-Regular", fontSize: Spacing[5] }}>
+        Open up App.js to start working on your app!
+      </Text>
       <Text style={{ marginTop: Spacing[5] }}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.darkOrange,
+    // backgroundColor: Colors.darkOrange,
     alignItems: "center",
     justifyContent: "center",
   },
