bc740296a83bd0e1dcdc02c38d38e1f304a1b4ae
Nazim-hasan <nazimhasan124073@gmail.com>
2022-05-26 00:55:30 +0600

    Spacing  added

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "./src/Theme/Colors";
+import { Spacing } from "./src/Theme/Spacing";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ marginTop: Spacing[5] }}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.darkOrange,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
